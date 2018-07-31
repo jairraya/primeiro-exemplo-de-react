265990482550fd1d6c92b2b17f57180b5a6e2ef8
jairrayas <jairrayas@outlook.com>
2018-07-31 11:22:58 -0300

    primeira aulda de react

---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+
+  clicar(){
+    prompt("Digite seu email para que possamos enchê-lo de SPAM e mailmarketing!");
+  }
+
+  render() {
+    return (
+        
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+
+  clicar(){
+    prompt("Digite seu email para que possamos enchê-lo de SPAM e mailmarketing!");
+  }
+
+  render() {
+    return (
+      <nav>
+        
+      </nav>
+    );
+  }
+}
+
+export default Footer;
