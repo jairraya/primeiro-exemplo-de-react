9e7b48ef21b8454cb9b7a4f7132d52d21d34b000
jairrayas <jairrayas@outlook.com>
2018-08-01 02:14:17 -0300

    app e index atualizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import FormLogin from './components/formlogin/FormLogin';
 
 class App extends Component {
   render() {
     return (
-      <Footer/>
+      <div>
+        <Header />
+        <FormLogin />
+        <Footer />
+      </div>
     )
   }
 }
